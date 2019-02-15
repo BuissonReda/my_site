683c490b5ac9d4baeaf2b5d1afa077378b6831df
Buisson Réda <buisson_reda@yahoo.fr>
2019-02-15 12:17:15 +0100

    wip 👨‍💻

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 
   <v-footer class="pa-3 footer">
-    <div>{{ t('lastUpdated') }} 12/2/2019, 21h50</div>
+    <div>{{ t('lastUpdated') }} 15/2/2019, 12h14</div>
     <a href="https://www.facebook.com/reda.buisson" class="fa fa-facebook"></a>
     <a href="https://twitter.com/BuissonRda" class="fa fa-twitter"></a>
     <a href="https://www.linkedin.com/in/r%C3%A9da-buisson-1a042b153/" class="fa fa-linkedin"></a>
